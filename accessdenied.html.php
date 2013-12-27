1722affef3c1003040ed2360c6a257c3af078e9d
Cyril Monta <cmonta@free.fr>
2013-12-28 00:08:22 +0100

    adding authentification

---
+++ accessdenied.html.php
@@ -0,0 +1,12 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . 'includes/helpers.inc.php'; ?>
+<!doctype html>
+<html lang="en">
+  <head
+    <meta charset="utf-8">
+    <title>Access denied</title>
+  </head>
+  <body>
+    <h1>Access denied</h1>
+    <p><?php htmlout($error); ?></p>
+  </body>
+</html>
--- members/index.php
+++ members/index.php
@@ -2,109 +2,127 @@
 
 include_once $_SERVER['DOCUMENT_ROOT'] .  '/includes/magicquotes.inc.php';
 
+// Vérifie si l'utilisateur a les habilitations
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/access.inc.php';
+
+if (!userIsLoggedIn())
+{
+  include '../login.html.php';
+  exit();
+}
+
+if (!userHasRole'Account Administrator'))
+{
+  $error = 'Only Account Administrators may access this page.';
+  include '../accessdenied.html.php';
+  exit();
+}
+
+
 // Affiche le formulaire pour ajouter un membre
 
 if (isset($_GET['add']))
-  {
-    $pageTitle = 'Nouveau membre';
-    $action = 'addform';
-    $name = '';
-    $email = '';
-    $id = '';
-    $button = 'Ajout membre';
+{
+  $pageTitle = 'Nouveau membre';
+  $action = 'addform';
+  $name = '';
+  $email = '';
+  $id = '';
+  $button = 'Ajout membre';
 
-    include 'form.html.php';
-    exit();
-  }
+  include 'form.html.php';
+  exit();
+}
 
 // Traite l'action du formulaire, ajoute un nouveau membre
 
 if (isset($_GET['addform']))
+{
+  include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
+
+  try
   {
-    include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
-
-    try
-      {
-	$sql = 'INSERT INTO member SET name = :name, email = :email';
-	$s = $pdo->prepare($sql);
-	$s->bindvalue(':name', $_POST['name']);
-	$s->bindvalue(':email', $_POST['email']);
-	$s->execute();
-      }
-    catch (PDOException $e)
-      {
-	$error = 'Error adding submitted member.';
-	include 'error.html.php';
-	exit();
-      }    
-
-    // renvoie à la page d'accueil
-    header('Location: .');
-    exit();
-
+    $sql = 'INSERT INTO member SET name = :name, email = :email';
+    $s = $pdo->prepare($sql);
+    $s->bindvalue(':name', $_POST['name']);
+    $s->bindvalue(':email', $_POST['email']);
+    $s->execute();
   }
+  catch (PDOException $e)
+  {
+    $error = 'Error adding submitted member.';
+    include 'error.html.php';
+    exit();
+  }
+
+  // renvoie à la page d'accueil
+  header('Location: .');
+  exit();
+
+}
 
 
 // Charge la page pour éditer les membres
 
 if (isset($_POST['action']) and $_POST['action'] == 'Edit')
+{
+  include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
+
+  try
   {
-    include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
-
-    try
-      {
-	$sql = 'SELECT id, name, email FROM member WHERE id = :id';
-	$s = $pdo->prepare($sql);
-	$s->bindValue(':id', $_POST['id']);
-	$s->execute();
-      }
-    catch (PDOException $e)
-      {
-	$error = 'Error fetching member details.';
-	include 'error.html.php';
-	exit();
-      }
-
-    $row = $s->fetch();
-    
-    $pageTitle = 'Editer membre';
-    $action = 'editform';
-    $name = $row['name'];
-    $email = $row['email'];
-    $id = $row['id'];
-    $button = 'Mettre à jour';
-
-    include 'form.html.php';
-    exit();
-
+    $sql = 'SELECT id, name, email FROM member WHERE id = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':id', $_POST['id']);
+    $s->execute();
   }
+  catch (PDOException $e)
+  {
+    $error = 'Error fetching member details.';
+    include 'error.html.php';
+    exit();
+  }
+
+  $row = $s->fetch();
+
+  $pageTitle = 'Editer membre';
+  $action = 'editform';
+  $name = $row['name'];
+  $email = $row['email'];
+  $id = $row['id'];
+  $button = 'Mettre à jour';
+
+  include 'form.html.php';
+  exit();
+
+}
 
 // Edite les membres
 
 if (isset($_GET['editform']))
+{
+  include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
+
+  try
   {
-    include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
-
-    try
-      {
-	$sql = 'UPDATE member SET name = :name, email = :email WHERE id = :id';
-	$s = $pdo->prepare($sql);
-	$s->bindValue(':id', $_POST['id']);
-	$s->bindValue(':name', $_POST['name']);
-	$s->bindValue(':email', $_POST['email']);
-	$s->execute();
-      }
-    catch (PDOException $e)
-      {
-	$error = 'Error updating submitted member!';
-	include 'error.html.php';
-	exit();
-      }
-
-    header('Location: .');
-    exit();
-
+    $sql = 'UPDATE member SET name = :name, email = :email WHERE id = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':id', $_POST['id']);
+    $s->bindValue(':name', $_POST['name']);
+    $s->bindValue(':email', $_POST['email']);
+    $s->execute();
   }
+  catch (PDOException $e)
+  {
+    $error = 'Error updating submitted member!';
+    include 'error.html.php';
+    exit();
+  }
+
+  header('Location: .');
+  exit();
+
+}
 
 // Gérer la suppression
 // supprimer les eventmember avec le memberid présent
@@ -112,42 +130,42 @@ if (isset($_GET['editform']))
 // il faudra vérifier si les events sont affectés à un member (à voir)
 
 if (isset($_POST['action']) and $_POST['action'] == 'Delete')
+{
+  include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
+
+  // Get and delete records in eventmember where member id is present
+  try
   {
-    include $_SERVER['DOCUMENT_ROOT'] . '/includes/dbrunhub.inc.php';
-
-    // Get and delete records in eventmember where member id is present
-    try
-      {
-	$sql = 'DELETE FROM eventmember WHERE memberid = :id';
-	$s = $pdo->prepare($sql);
-	$s->bindValue(':id', $_POST['id']);
-	$s->execute();
-      }
-    catch (PDOException $e)
-      {
-	$error = 'Error deleting member events.';
-	include 'error.html.php';
-	exit();
-      }
-
-    // Delete the member
-    try
-      {
-	$sql = 'DELETE FROM member WHERE id = :id';
-	$s = $pdo->prepare($sql);
-	$s->bindValue(':id', $_POST['id']);
-	$s->execute();
-      }
-    catch (PDOException $e)
-      {
-	$error = 'Error deleting member.';
-	include 'error.html.php';
-	exit();
-      }
-
-    header('Location: .');
+    $sql = 'DELETE FROM eventmember WHERE memberid = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':id', $_POST['id']);
+    $s->execute();
+  }
+  catch (PDOException $e)
+  {
+    $error = 'Error deleting member events.';
+    include 'error.html.php';
     exit();
   }
+
+  // Delete the member
+  try
+  {
+    $sql = 'DELETE FROM member WHERE id = :id';
+    $s = $pdo->prepare($sql);
+    $s->bindValue(':id', $_POST['id']);
+    $s->execute();
+  }
+  catch (PDOException $e)
+  {
+    $error = 'Error deleting member.';
+    include 'error.html.php';
+    exit();
+  }
+
+  header('Location: .');
+  exit();
+}
 
 
 
@@ -168,9 +186,9 @@ catch (PDOException $e)
 }
 
 foreach ($result as $row)
-  {
-    $members[] = array('id' => $row['id'], 'name' => $row['name'], 'email' => $row['email']);
-  }
+{
+  $members[] = array('id' => $row['id'], 'name' => $row['name'], 'email' => $row['email']);
+}
 
 
 
