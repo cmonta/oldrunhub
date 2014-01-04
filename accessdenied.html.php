6d6dfbea1502f2bb3c4a129f40f6b0629fd2ce2e
Cyril Monta <cmonta@free.fr>
2014-01-04 19:20:37 +0100

    rajout

---
+++ accessdenied.html.php
@@ -0,0 +1,13 @@
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/helpers.inc.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Access denied</title>
+  </head>
+  <body>
+    <h1>Access denied</h1>
+    <p><?php htmlout($error); ?></p>
+    <p><a href="..">Retournez à la page d’accueil</a></p>
+  </body>
+</html>
